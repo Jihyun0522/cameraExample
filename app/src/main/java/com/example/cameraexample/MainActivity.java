daf87d4a196b35ea7b109b39a601018eb72c5ef1
jihyun0522 <s2017s01@e-mirim.hs.kr>
2019-05-24 00:52:48 +0900

    Success in creating and saving storage. A front camera

--- app/src/main/java/com/example/cameraexample/MainActivity.java
+++ app/src/main/java/com/example/cameraexample/MainActivity.java
@@ -2,19 +2,26 @@ package com.example.cameraexample;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.media.MediaRecorder;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,22 +31,45 @@ public class MainActivity extends AppCompatActivity {
     SurfaceHolder holder;
     MediaRecorder recorder;
 
-    String path = "/sdcard/DCIM/Camera/recorded_video.mp4";
+    Camera camera;
+
+    SimpleDateFormat format = new SimpleDateFormat("yyMMdd_HHmmss");
+
+    long now;
+    Date time;
+    String time_name;
+    String path;
+
+    private String getTime() {
+        now = System.currentTimeMillis();
+        time = new Date(now);
+        return format.format(time);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String str = Environment.getExternalStorageState();
+        if (str.equals(Environment.MEDIA_MOUNTED)) {
+
+            String dirPath = "/sdcard/DCIM/모면";
+            File file = new File(dirPath);
+            if (!file.exists())  // 원하는 경로에 폴더가 있는지 확인
+                file.mkdirs();
+        } else
+            Toast.makeText(this, "SD Card 인식 실패", Toast.LENGTH_SHORT).show();
+
         int permssionCamera = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
         int permssionRecord = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
         int permssionRead = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
         int permssionWrite = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
-        if((permssionCamera != PackageManager.PERMISSION_GRANTED) && (permssionRecord != PackageManager.PERMISSION_GRANTED) && (permssionRead != PackageManager.PERMISSION_GRANTED) && (permssionWrite != PackageManager.PERMISSION_GRANTED)) {
+        if ((permssionCamera != PackageManager.PERMISSION_GRANTED) && (permssionRecord != PackageManager.PERMISSION_GRANTED) && (permssionRead != PackageManager.PERMISSION_GRANTED) && (permssionWrite != PackageManager.PERMISSION_GRANTED)) {
             Toast.makeText(this, "권한 승인이 필요합니다.", Toast.LENGTH_LONG).show();
 
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA) && ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO) && ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE) && ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA) && ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO) && ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE) && ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
                 Toast.makeText(this, "면접을 녹화하기 위해 카메라, 오디오 녹음 등의 권한이 필요합니다.", Toast.LENGTH_LONG).show();
             } else {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSIONS_ALL);
@@ -47,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        surfaceView = (SurfaceView)findViewById(R.id.surfaceView);
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
 
         holder = surfaceView.getHolder();
     }
@@ -55,14 +85,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
-            case PERMISSIONS_ALL : {
+            case PERMISSIONS_ALL: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-                    Toast.makeText(this,"승인이 허가되어 있습니다.",Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, "승인이 허가되어 있습니다.", Toast.LENGTH_LONG).show();
 
                 } else {
-                    Toast.makeText(this,"아직 승인받지 않았습니다.",Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, "아직 승인받지 않았습니다.", Toast.LENGTH_LONG).show();
                 }
                 return;
             }
@@ -70,9 +100,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void onBtn1Clicked(View v){
+    public void onBtn1Clicked(View v) {
+        time_name = getTime();
+
         try {
-            if(recorder != null){
+            if (recorder != null) {
                 recorder.stop();
                 recorder.release();
                 recorder = null;
@@ -80,11 +112,19 @@ public class MainActivity extends AppCompatActivity {
 
             recorder = new MediaRecorder();
 
+            camera = Camera.open(1);
+            camera.setDisplayOrientation(90);
+            camera.unlock();
+
+            recorder.setCamera(camera);
+
             recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
             recorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
             recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
             recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
             recorder.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT);
+
+            path = "/sdcard/DCIM/모면/" + time_name + ".mp4";
 
             recorder.setOutputFile(path);
             recorder.setPreviewDisplay(holder.getSurface());
@@ -93,16 +133,19 @@ public class MainActivity extends AppCompatActivity {
 
             Toast.makeText(getApplicationContext(), "녹화를 시작합니다.", Toast.LENGTH_LONG).show();
 
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void onBut2Clicked(View v) {
-        if(recorder != null) {
+        if (recorder != null) {
             recorder.stop();
             recorder.release();
             recorder = null;
+
+            camera.stopPreview();
+            camera.release();
         }
 
         Toast.makeText(getApplicationContext(), "녹화를 중지합니다.", Toast.LENGTH_LONG).show();
